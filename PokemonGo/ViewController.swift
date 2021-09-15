d053ff5281af620b621fdf41f29e5633c7473149
Andre Linces <andrelinces@yahoo.com.br>
2021-09-15 18:47:53 -0300

    Inserindo map view, configurando constraints.

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var mapa: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
