5ae9fa20630501d0b03dfc9982c61e1a09fefb98
Andre Linces <andrelinces@yahoo.com.br>
2021-09-15 19:49:55 -0300

    Solicitando permissão de localização e tratando quando é negada.

--- PokemonGo/ViewController.swift
+++ PokemonGo/ViewController.swift
@@ -9,17 +9,52 @@ import UIKit
 import MapKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    
+
     @IBOutlet weak var mapa: MKMapView!
+    
+    var gerenciadorLocalizacao = CLLocationManager()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mapa.delegate = self
+        //O Objeto vai ser gerenciado pela própria classe viewController.
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()
+        gerenciadorLocalizacao.startUpdatingLocation()
+        
     }
-
+    //Método para verificar quando usuário nega permissão de acesso a localização.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if status != .authorizedWhenInUse && status != .notDetermined{
+            
+            //Alerta
+            let alertController = UIAlertController(title: "Permissão de localização !",
+                                                    message: "Para que você possa caçar pokemons, precisamos da sua localização, por favor, Habilite",
+                                                    preferredStyle: .alert)
+            
+            let acaoConfiguracoes = UIAlertAction(title: "Acessar Configurações?", style: .default) { (alertaConfiguracoes) in
+                
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString){
+                    
+                    UIApplication.shared.open(configuracoes as URL )
+                    
+                }
+             
+            }
+            
+            let acaoCancelar = UIAlertAction(title: "Cancelar ?", style: .default, handler: nil)
+            
+            alertController.addAction(acaoConfiguracoes)
+            alertController.addAction(acaoCancelar)
+            
+        }
+        
+    }
 
 }
 
