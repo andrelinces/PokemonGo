f7f0dab87ed6ca5c0334dce5d3aafff19fb9c370
Andre Linces <andrelinces@yahoo.com.br>
2021-09-16 20:52:50 -0300

    Criando CoreData e classe para gerenciar o coredata.

--- PokemonGo/CoreDataPokemon.swift
+++ PokemonGo/CoreDataPokemon.swift
@@ -22,11 +22,34 @@ class CoreDataPokemon {
     //Adicionar todos os pokemons
     func adicionarPokemons(){
         
+        let context = self.getContex()
+        
+    
+        self.criarPokemon(nome: "Mew", nomeImage: "mew", capturado: false)
+        self.criarPokemon(nome: "Caterpie", nomeImage: "caterpie", capturado: false)
+        self.criarPokemon(nome: "Charmander", nomeImage: "charmander", capturado: false)
+        self.criarPokemon(nome: "Pikachu", nomeImage: "pikachu-2", capturado: true)
+        //self.criarPokemon(nome: "Mew", nomeImage: "mew", capturado: false)
+        
+        do {
+            try context.save()
+        } catch  {
+            
+        }
+        
         
     }
     
     
     //Criar os pokemons
-    
+    func criarPokemon( nome: String, nomeImage: String, capturado: Bool ){
+        
+        let context = self.getContex()
+        let pokemon = Pokemon.init(context: context)
+        pokemon.nome = nome
+        pokemon.nomeImagem = nomeImage
+        pokemon.capturado = capturado
+        
+    }
     
 }
